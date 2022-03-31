6d30cfa10f527e2b935db75aa09e74df24dc1da4
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-03-31 21:33:45 +0300

    feat: integrate pokemonType with pokemon model

--- pokemon/packages/pokemon_repo/lib/src/models/pokemon_model.dart
+++ pokemon/packages/pokemon_repo/lib/src/models/pokemon_model.dart
@@ -1,17 +1,18 @@
 import 'pokemon_stat.dart';
+import 'pokemon_type.dart';
 
 typedef PokemonId = int;
 
 class Pokemon {
   final PokemonId id;
   final String name;
-  final List<String> types;
   final String imageUrl;
-  final List<PokemonStat> stats;
   final int height;
   final int weight;
   final double bmi;
   final double avgPower;
+  final List<PokemonType> types;
+  final List<PokemonStat> stats;
 
   const Pokemon._({
     required this.id,
@@ -29,7 +30,7 @@ class Pokemon {
     required PokemonId id,
     required String imageUrl,
     required String name,
-    required List<String> types,
+    required List<PokemonType> types,
     required List<PokemonStat> stats,
     required int height,
     required int weight,
@@ -53,7 +54,7 @@ class Pokemon {
         id: json['id'],
         imageUrl: json['imageUrl'],
         name: json['name'],
-        types: json['types'],
+        types: (json['types'] as List).map((e) => PokemonType.fromString(name: e)).toList(),
         stats: (json['stats'] as List).map((e) => PokemonStat.fromJson(e)).toList(),
         height: json['height'],
         weight: json['weight'],
@@ -62,7 +63,7 @@ class Pokemon {
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
-        'types': types,
+        'types': types.map((e) => e.name).toList(),
         'imageUrl': imageUrl,
         'stats': stats.map((e) => e.toJson()).toList(),
         'height': height,
