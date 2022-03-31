40bfdbd82b34bc05ebfa81b06ea33ac01ff332bc
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-03-31 22:27:57 +0300

    feat: implement getPokemon func

--- pokemon/packages/pokemon_repo/lib/src/pokemon_repo.dart
+++ pokemon/packages/pokemon_repo/lib/src/pokemon_repo.dart
@@ -11,9 +11,42 @@ class PokemonRepo {
         _storageHandler = storageHandler;
 
   Future<Pokemon> getPokemon(PokemonId id) async {
-    final Map<String, dynamic> pokemonJson = await _storageHandler.readPokemon(id) ?? (await _httpHandler.fetchPokemonDetail(id));
+    final cachedPokemon = await _readPokemon(id);
 
-    return Pokemon.fromJson(pokemonJson);
+    if (cachedPokemon != null) {
+      return cachedPokemon;
+    }
+
+    final pokemonJson = await _httpHandler.fetchPokemonDetail(id);
+    final imageUrl = pokemonJson['sprites']['other']['official-artwork']['front_default'];
+    final stats = <PokemonStat>[];
+    final types = <PokemonType>[];
+    for (final statJson in pokemonJson['stats']) {
+      stats.add(
+        PokemonStat(
+          name: statJson['stat']['name'],
+          value: statJson['base_stat'],
+        ),
+      );
+    }
+
+    for (final typeJson in pokemonJson['types']) {
+      types.add(
+        PokemonType.fromString(
+          name: typeJson['type']['name'],
+        ),
+      );
+    }
+
+    return Pokemon(
+      id: pokemonJson['id'],
+      imageUrl: imageUrl,
+      name: pokemonJson['name'],
+      types: types,
+      stats: stats,
+      height: pokemonJson['height'],
+      weight: pokemonJson['weight'],
+    );
   }
 
   Future<PokemonPaginationResponse> getPokemonsToFetch({required int limit, required int offset}) async {
@@ -42,5 +75,19 @@ class PokemonRepo {
 
   Future<List<PokemonId>> getFavoritePokemons() async {
     return await _storageHandler.getFavorites();
+  }
+
+  Future<Pokemon?> _readPokemon(PokemonId id) async {
+    try {
+      final pokemonJson = await _storageHandler.readPokemon(id);
+      if (pokemonJson != null) {
+        return Pokemon.fromJson(pokemonJson);
+      }
+
+      return null;
+    } catch (_) {
+      // TODO log error
+      return null;
+    }
   }
 }
