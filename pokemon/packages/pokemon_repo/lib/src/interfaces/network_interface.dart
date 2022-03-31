76680217f9a9f4585e1cfb6d79f4eabf2ee4551a
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-03-31 22:06:07 +0300

    feat: update fetchPokemons return type

--- pokemon/packages/pokemon_repo/lib/src/interfaces/network_interface.dart
+++ pokemon/packages/pokemon_repo/lib/src/interfaces/network_interface.dart
@@ -2,4 +2,5 @@ import '../models/models.dart';
 
 abstract class PokemonHttpHandler {
   Future<Map<String, dynamic>> fetchPokemonDetail(PokemonId id);
+  Future<Map<String, dynamic>> fetchPokemons({required int limit, required int offset});
 }
