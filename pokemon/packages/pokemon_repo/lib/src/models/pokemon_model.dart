6a0fa9359ab3399c11b7764d46532de37c9d39bc
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-03-31 21:19:37 +0300

    feat: add remaining properties to pokemon model

--- pokemon/packages/pokemon_repo/lib/src/models/pokemon_model.dart
+++ pokemon/packages/pokemon_repo/lib/src/models/pokemon_model.dart
@@ -6,14 +6,46 @@ class Pokemon {
   final List<String> types;
   final String imageUrl;
   final List<PokemonStat> stats;
+  final int height;
+  final int weight;
+  final double bmi;
+  final double avgPower;
 
-  const Pokemon({
+  const Pokemon._({
     required this.id,
     required this.imageUrl,
     required this.name,
     required this.types,
     required this.stats,
+    required this.height,
+    required this.weight,
+    required this.avgPower,
+    required this.bmi,
   });
+
+  factory Pokemon({
+    required int id,
+    required String imageUrl,
+    required String name,
+    required List<String> types,
+    required List<PokemonStat> stats,
+    required int height,
+    required int weight,
+  }) {
+    final bmi = weight / (height * height);
+    final avgPower = stats.fold<int>(0, (previousValue, element) => previousValue + element.value) / stats.length;
+    return Pokemon._(
+      id: id,
+      imageUrl: imageUrl,
+      name: name,
+      types: types,
+      stats: stats,
+      height: height,
+      weight: weight,
+      avgPower: avgPower,
+      bmi: bmi,
+    );
+  }
 
   factory Pokemon.fromJson(Map<String, dynamic> json) => Pokemon(
         id: json['id'],
@@ -21,6 +53,8 @@ class Pokemon {
         name: json['name'],
         types: json['types'],
         stats: (json['stats'] as List).map((e) => PokemonStat.fromJson(e)).toList(),
+        height: json['height'],
+        weight: json['weight'],
       );
 
   Map<String, dynamic> toJson() => {
@@ -29,5 +63,7 @@ class Pokemon {
         'types': types,
         'imageUrl': imageUrl,
         'stats': stats.map((e) => e.toJson()).toList(),
+        'height': height,
+        'weight': weight,
       };
 }
