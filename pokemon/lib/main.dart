1fe6dab073a78277cd56d735f547e1dadef3d452
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-04-02 01:14:11 +0300

    feat: dart error handling

--- pokemon/lib/main.dart
+++ pokemon/lib/main.dart
@@ -1,12 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'src/app.dart';
+import 'src/bloc_observer.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
+  WidgetsFlutterBinding.ensureInitialized();
+
   final settingsController = SettingsController(SettingsService());
 
   // Load the user's preferred theme while the splash screen is displayed.
@@ -16,5 +22,17 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  runZonedGuarded(
+    () {
+      BlocOverrides.runZoned(
+        () {
+          runApp(MyApp(settingsController: settingsController));
+        },
+        blocObserver: MyBlocObserver(),
+      );
+    },
+    (error, stack) {
+      //TODO log dart errors
+    },
+  );
 }
