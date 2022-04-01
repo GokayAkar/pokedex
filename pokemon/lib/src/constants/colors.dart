67d8f44f3fa97f2db6d1f02f973573afb7e54b79
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-04-02 01:39:46 +0300

    feat: add white text color

--- pokemon/lib/src/constants/colors.dart
+++ pokemon/lib/src/constants/colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static const introBackgroundColor = Color(0xff3558CD);
   static const textColorBlack = Color(0xff161A33);
   static const textColorGrey = Color(0xff6B6B6B);
+  static const textColorWhite = Color(0xffFFFFFF);
 }
