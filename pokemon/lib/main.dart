d55e645c7f6696f29bfd61a10722d77735993077
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-04-02 02:40:29 +0300

    feat: create repo and source of truth pokedex before runApp

--- pokemon/lib/main.dart
+++ pokemon/lib/main.dart
@@ -2,16 +2,31 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokemon_api/pokemon_repo.dart';
 
 import 'src/app.dart';
 import 'src/utils/bloc_observer.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   WidgetsFlutterBinding.ensureInitialized();
+
+  final _pokemonRepo = PokemonRepo(
+    storageHandler: SharedPreferencesCacheHandler(
+      prefs: await SharedPreferences.getInstance(),
+    ),
+  );
+
+  //this map will hold all references for in memory pokemons
+  //whenever we get a pokemon we will register it in this map
+  //before get a pokemon we will look up to this map
+  //because otherwise since allPokemons and favourites gonna work seperately
+  //they can get same pokemon and we can hold 2 different ref for same pokemon
+  final _pokedex = <PokemonId, Pokemon>{};
 
   final settingsController = SettingsController(SettingsService());
 
@@ -26,7 +41,19 @@ void main() async {
     () {
       BlocOverrides.runZoned(
         () {
-          runApp(MyApp(settingsController: settingsController));
+          runApp(
+            MultiRepositoryProvider(
+              providers: [
+                RepositoryProvider<PokemonRepo>.value(
+                  value: _pokemonRepo,
+                ),
+                RepositoryProvider<Map<PokemonId, Pokemon>>.value(
+                  value: _pokedex,
+                ),
+              ],
+              child: MyApp(settingsController: settingsController),
+            ),
+          );
         },
         blocObserver: MyBlocObserver(),
       );
