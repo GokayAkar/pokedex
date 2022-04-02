ec4c7ba1aec4b6e5d210b8a772dca789d05c48c4
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-04-02 15:50:28 +0300

    feat : add new constant color

--- pokemon/lib/src/constants/colors.dart
+++ pokemon/lib/src/constants/colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   const AppColors._();
 
   static const introBackgroundColor = Color(0xff3558CD);
+  static const homeBackgroundColor = Color(0xffE8E8E8);
   static const textColorBlack = Color(0xff161A33);
   static const textColorGrey = Color(0xff6B6B6B);
   static const textColorWhite = Color(0xffFFFFFF);
