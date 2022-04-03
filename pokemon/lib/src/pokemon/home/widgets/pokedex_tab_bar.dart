97b2d2ab7a7139e66995c356e32eb79e5bd591a5
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-04-03 05:03:35 +0300

    feat: add favourite pokemon count counter to tabbar

--- pokemon/lib/src/pokemon/home/widgets/pokedex_tab_bar.dart
+++ pokemon/lib/src/pokemon/home/widgets/pokedex_tab_bar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pokemon/src/constants/constants.dart';
 import 'package:pokemon/src/localization/l10n.dart';
+import 'package:pokemon/src/pokemon/favourite_pokemons/cubits/favourite_pokemons_cubit.dart';
 
 class PokedexTabBar extends StatelessWidget implements PreferredSizeWidget {
   const PokedexTabBar({Key? key}) : super(key: key);
@@ -23,7 +25,39 @@ class PokedexTabBar extends StatelessWidget implements PreferredSizeWidget {
             text: context.l10n.allPokemons,
           ),
           Tab(
-            text: context.l10n.favourites,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  context.l10n.favourites,
+                ),
+                5.horizontalSpace,
+                BlocBuilder<FavouritePokemonsCubit, FavouritePokemonsState>(
+                  builder: (context, state) {
+                    return Visibility(
+                      visible: state.favouritePokemons.isNotEmpty,
+                      child: Container(
+                        width: 20.w,
+                        height: 20.h,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.introBackgroundColor,
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          state.favouritePokemons.length.toString(),
+                          style: TextStyle(
+                            color: AppColors.textColorWhite,
+                            fontSize: FontSizes.tiny,
+                            fontWeight: FontWeights.w400,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                )
+              ],
+            ),
           ),
         ],
         indicatorWeight: 4.h,
