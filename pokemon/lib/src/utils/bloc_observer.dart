e84d12bc370ac5ab22a2f3fe2e126bacea88bd5d
Gokay <85453255+GokayFinfree@users.noreply.github.com>
2022-04-03 15:12:41 +0300

    remove: unneccesary comments

--- pokemon/lib/src/utils/bloc_observer.dart
+++ pokemon/lib/src/utils/bloc_observer.dart
@@ -1,39 +1,33 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyBlocObserver extends BlocObserver {
-  @override
-  void onCreate(BlocBase bloc) {
-    //log something if necessary
-    super.onCreate(bloc);
-  }
+  // @override
+  // void onCreate(BlocBase bloc) {
+  //   super.onCreate(bloc);
+  // }
 
-  @override
-  void onEvent(Bloc bloc, Object? event) {
-    //log something if necessary
-    super.onEvent(bloc, event);
-  }
+  // @override
+  // void onEvent(Bloc bloc, Object? event) {
+  //   super.onEvent(bloc, event);
+  // }
 
-  @override
-  void onChange(BlocBase bloc, Change change) {
-    //log something if necessary
-    super.onChange(bloc, change);
-  }
+  // @override
+  // void onChange(BlocBase bloc, Change change) {
+  //   super.onChange(bloc, change);
+  // }
 
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    //log something if necessary
-    super.onTransition(bloc, transition);
-  }
+  // @override
+  // void onTransition(Bloc bloc, Transition transition) {
+  //   super.onTransition(bloc, transition);
+  // }
 
-  @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    //log something if necessary
-    super.onError(bloc, error, stackTrace);
-  }
+  // @override
+  // void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+  //   super.onError(bloc, error, stackTrace);
+  // }
 
-  @override
-  void onClose(BlocBase bloc) {
-    //log something if necessary
-    super.onClose(bloc);
-  }
+  // @override
+  // void onClose(BlocBase bloc) {
+  //   super.onClose(bloc);
+  // }
 }
